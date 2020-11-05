76fe90755b040225bb26852aa500c32da1613e6f
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-11-05 22:05:22 +0000

    Functional Functions components

--- script.js
+++ script.js
@@ -2,29 +2,53 @@ const ClassName = {
     container : 'container-fluid',
     row : 'row',
     header : 'col-12 header',
-    title : 'title mt-5 text-center',
+    header_title : 'header_title mt-5 text-center',
     image : './logo.png', // link,
-    logo : 'logo mx-auto'
+    logo : 'logo mx-auto',
+
+    main : 'col-12 main',
+    main_title : 'main_title mt-5 text-center'
+
 
 }
 
-class Header extends React.Component{
+function Header(){
 
-    render () {
-       return   <div className={ClassName.container} >
+
+       return   <div className={ClassName.container}>
                    <div className={ClassName.row}>
                        <div className={ClassName.header}>
-                            <h1 className={ClassName.title}>HELLO REACT ! </h1>
+                            <h1 className={ClassName.header_title}>HELLO REACT ! </h1>
                            <img src={ClassName.image} alt="logo"  className={ClassName.logo}/>
                        </div>
                    </div>
            </div>
 
-    }
+    
+}
+
+
+function Main(){
+
+        return <div className={ClassName.container}>
+                    <div className={ClassName.row}>
+                        <div className={ClassName.main}>
+                            <h1 className={ClassName.main_title}>MAIN PART ! </h1>
+                        </div>
+
+
+                    </div>
+        </div>
+
 }
 
 
 ReactDOM.render(
     < Header />,
     document.getElementById('header')
+)
+
+ReactDOM.render(
+    < Main />,
+    document.getElementById('main')
 )
